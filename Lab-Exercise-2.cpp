7a6b9b3ad8db6551ed371897437da116f0c04118
renniseyn <33915462+renniseyn@users.noreply.github.com>
2018-01-18 09:29:17 +0800

    i tried again..

--- Lab-Exercise-2.cpp
+++ Lab-Exercise-2.cpp
@@ -1,33 +1,38 @@
+//Leang Yue Nin
+//A17DW0182
 //Check whether a number is prime or not
 //finish up this program, properly, and nicely.
 
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-int main()
-{
-
+int main() {
+	
 	int i=1,u=1,sum=0;
 	cout<<"The perfect numbers between 1 to 500 are: "<<endl;
-
-	while(i<=500)
-
-		while(u<=500)
-
+	
+	while(i<500)
+	{
+		while(u<500)
+		{
 			if(u<i)
-
-				if(i%u=)
+			{
+				if(i%u==0)
+				{
 					sum=sum+u;
-
-	u++
-
-		if(sum=i)
-			
-			cout<<i<<" "<<endl;
-
-	i++
-		u=1;
-		sum=0;
-
-
+					u++;
+				}
+			}
+		}
+	}
+	
+	if(sum==i)
+		{
+			cout<<i<<"is a perfect number."<<endl;
+			i++;
+			u=1;
+			sum=0;
+		}
+		
+	return 0;
 }
