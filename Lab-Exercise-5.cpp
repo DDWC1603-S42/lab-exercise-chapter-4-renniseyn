ca63a3655f384298d21a910556d27d006cadb3db
renniseyn <33915462+renniseyn@users.noreply.github.com>
2018-01-19 22:02:51 +0800

    I think it's correct ady

--- Lab-Exercise-5.cpp
+++ Lab-Exercise-5.cpp
@@ -1,3 +1,5 @@
+//Leang Yue Nin
+//A17DW0182
 //Get out from this program
 //find a way to fix and complete this program. good luck.
 
@@ -5,17 +7,19 @@
 using namespace std;
 int main()
 {
-	while(false)//infinite loop
+	while(true)//infinite loop
 	{
 		std::cout<<"Enter 0 to exit or anyhing else to continue: ";
 		int num;
-		cin>>num;	
+		std::cin>>num;	
 
 		//exit loop if user enters
 		if(num==0)
 			break;
 	}
-	std::cout<<"we are out";
+	
+	std::cout<<"we are out"<<endl;
 
 	return 0;
 }
+
