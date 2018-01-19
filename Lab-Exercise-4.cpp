f3bb142f6eb72902241c37dbced7ac44557e6aae
renniseyn <33915462+renniseyn@users.noreply.github.com>
2018-01-19 14:01:42 +0800

    i will check again later

--- Lab-Exercise-4.cpp
+++ Lab-Exercise-4.cpp
@@ -15,13 +15,16 @@ int main()
 
 	int i,j,rows;
 	cout<<"Input number of rows :"<<endl;
+	cin>>rows;
 
 
 	for(i=1;i<=rows;i++)
 	{
-	for(j=1;j<=i;j++)
+		for(j=1;j<=i;j++)
 		
-	cout<<endl;
+		cout<<i<<endl;
+		cout<<j<<endl;
+
 	}
 }
 	
